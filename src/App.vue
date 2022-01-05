c6187fcfaacbda8ba0996934285c9b3288f132b8
hpsuperman <2430571847@qq.com>
2022-01-05 22:26:21 +0800

    依赖的school

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <transition name="router-fade" mode="out-in">
+			<keep-alive>
+    <router-view v-if="$route.meta.keepAlive"></router-view>
+			</keep-alive>
+      </transition>
+       <transition name="router-fade" mode="out-in">
+			<router-view v-if="!$route.meta.keepAlive"></router-view>
+		</transition>
+    <svg-icon></svg-icon>	
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+<style lang="scss">
+  #app {}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import routes from '../src/router/index'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+Vue.use(Antd);
+const router = new VueRouter({
+  routes,
+})
+new Vue({
+  el:'#app',
+  router,
+  render: h=> h(App)
+}).$mount('#app')
